7b158dbf244f8d0ce58cbfc716c861560e9a6d14
Anton Dodonov <cpodimas@gmail.com>
2023-06-14 12:51:15 +0700

    c:

--- content/webarchive/1_webarchive_scrapper.sh
+++ content/webarchive/1_webarchive_scrapper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Путь к файлу с доменами
-file="domains.txt"
+file="webarch-data/domains.txt"
 
 # Проверка наличия файла
 if [ ! -f "$file" ]; then
@@ -13,7 +13,7 @@ fi
 while IFS= read -r line; do
     # Извлечение имени домена до первого пробела или точки с запятой
     DOMAIN=$(echo "$line" | awk -F '[ ;]' '{print $1}')
-    CMD="python3 webarchive_scrapper -f webarchive $DOMAIN"
+    CMD="python3 webarchive_scrapper -f webarch-data/web.archive.org $DOMAIN"
     echo $CMD
     $CMD
 done < "$file"
