cc8068405d4f3fa67ebfe44670d2fec2e8260c20
Your Name <you@example.com>
2023-06-26 19:10:46 +0700

    c:

--- domains/check_domains/check_domains_google/start2.sh
+++ domains/check_domains/check_domains_google/start2.sh
@@ -1,1 +1,2 @@
-nohup python3 check_domains_google data/pbn-data/domains/info/info0_exp.txt data/pbn-data/domains/info/info0_exp_ind.txt data/pbn-data/domains/info/info0_exp_noind.txt
+#nohup
+ python3 check_domains_google data/pbn-data/domains/github_all_nc.txt data/pbn-data/domains/info/info0_exp_ind.txt data/pbn-data/domains/github_all_nc_whois.txt
