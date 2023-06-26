1a5e0cc93c2786a9be2d8542b66691d35f90b8e2
Anton Dodonov <cpodimas@gmail.com>
2023-06-26 17:46:21 +0700

    c:

--- domains/check_domains/check_domains_whois/start.sh.2.sh
+++ domains/check_domains/check_domains_whois/start.sh.2.sh
@@ -1,5 +1,7 @@
 #nohup python3 ddec.py -f pbn-data/commoncrawl_domains_2015_nc.txt -c -i 0 -oe >> pbn_data/commoncrawl_domains_2015_whois_checked.txt 
-nohup  python3 ddec.py -f data/domain-data/github_all_nc.txt -c -i 0 -oe >> data/domain-data/github_all_nc_whois.txt &
+#nohup 
+ python3 ddec.py -f data/domains-data/domains/github/github_all_nc.txt -c -i 0 -oe >> data/domains-data/domains/github/github_all_nc_whois.txt
+# &
 #nohup python3 ddec.py -f src/gov_delhi.txt -c -i 0 -oe  >> dst/gov_delhi.txt &
 #nohup python3 ddec.py -f src/gov_hp.txt -c -i 0 -oe  >> dst/gov_hp.txt &
 #nohup python3 ddec.py -f src/gov_karnataka.txt -c -i 0 -oe  >> dst/gov_karnataka.txt &
