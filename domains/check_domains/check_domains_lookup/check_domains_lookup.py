57eaf3bfcd91dd51f2d532dee0c04da129affb54
Anton Dodonov <cpodimas@gmail.com>
2023-06-26 15:24:12 +0700

    c:

--- domains/check_domains/check_domains_lookup/check_domains_lookup.py
+++ domains/check_domains/check_domains_lookup/check_domains_lookup.py
@@ -3,8 +3,8 @@ import socket
 import tldextract
 
 NUM_WORKERS = 100
-FILE_SRC="data/domains-data/github_all.txt"
-FILE_DST="data/domains-data/github_all_nc.txt"
+FILE_SRC="data/domains-data/domains/github/github_all.txt"
+FILE_DST="data/domains-data/domains/github/github_all_nc.txt"
 
 
 def check_domain(line):
