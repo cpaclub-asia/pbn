45cfa3551da16b6036cdd5309b553d91690ce9aa
Anton Dodonov <cpodimas@gmail.com>
2023-06-28 09:46:44 +0700

    c:

--- domains/stat_domains/check_domains_google/start.sh
+++ domains/stat_domains/check_domains_google/start.sh
@@ -2,9 +2,4 @@
 
 #python3 check_domains_google pbn-data/domains/info/info2_exp.txt pbn-data/domains/info/info2_exp_ind.txt pbn-data/domains/info/info2_exp_noind.txt
 
-<<<<<<< HEAD
-python3 check_domains_google pbn-data/domains/info/info0_exp.txt pbn-data/domains/info/info0_exp_ind.txt pbn-data/domains/info/info0_exp_noind.txt
-=======
-python3 check_domains_google data/pbn-data/domains/info/info0_exp.txt data/pbn-data/domains/info/info0_exp_ind.txt data/pbn-data/domains/info/info0_exp_noind.txt
->>>>>>> a19cce70cdaf37d3429495d007fe512371f4f861
-#&
+python3 check_domains_google data/domains-data/domains/name.txt data/domains-data/domains/name_ind.txt data/domains-data/domains/name_noind.txt
