72dc910e919dbfa7097e06b3b64ad93015f6dcf9
Anton Dodonov <cpodimas@gmail.com>
2023-07-04 09:38:22 +0700

    c:

--- content/webarchive/funcs/webarchive/html_utils.py
+++ content/webarchive/funcs/webarchive/html_utils.py
@@ -9,45 +9,25 @@ def remove_web_archive_links(soup):
         css_links = soup.find_all('link', attrs={'rel': 'stylesheet', 'href': lambda href: href and '_static' in href})
         for link in css_links:
             link.extract()
-        links = head_tag.find_all()
-        for tag in links:
-            for attr, value in tag.attrs.items():
-                if isinstance(value, str):
-                    updated_value = re.sub(r'https://web\.archive\.org/web/\d+/', '', value)
-                    updated_value = re.sub(r'https://web\.archive\.org/web/\d+im_/', '', updated_value)
-                    updated_value = re.sub(r'https://web\.archive\.org/web/\d+cs_/', '', updated_value)
-                    updated_value = re.sub(r'//web\.archive\.org/web/\d+/', '', value)
-                    updated_value = re.sub(r'//web\.archive\.org/web/\d+im_/', '', updated_value)
-                    updated_value = re.sub(r'//web\.archive\.org/web/\d+cs_/', '', updated_value)
+        
+    tags = soup.find_all()
+    for tag in tags:
+        for attr, value in tag.attrs.items():
+            if isinstance(value, str):
+                updated_value = re.sub(r'https://web\.archive\.org/web/\d+/', '', value)
+                updated_value = re.sub(r'https://web\.archive\.org/web/\d+im_/', '', updated_value)
+                updated_value = re.sub(r'https://web\.archive\.org/web/\d+cs_/', '', updated_value)
+                #https://web.archive.org/web/20230130234931im_
+                updated_value = re.sub(r'//web\.archive\.org/web/\d+/', '', value)
+                updated_value = re.sub(r'//web\.archive\.org/web/\d+im_/', '', updated_value)
+                updated_value = re.sub(r'//web\.archive\.org/web/\d+cs_/', '', updated_value)
 
-                    updated_value = re.sub(r'/web/\d+/(http\S+)/', r'\1', updated_value)
+                updated_value = re.sub(r'/web/\d+/', '', updated_value)
+                updated_value = re.sub(r'/web/\d+im_/', '', updated_value)
+                updated_value = re.sub(r'/web/\d+cs_/', '', updated_value)
 
-                    tag[attr] = updated_value
+                tag[attr] = updated_value
 
-
-    # Удаление префикса "https://web.archive.org/web/XXXXXXXXXXXXXX/" из ссылок и атрибутов изображений и формы внутри <body>
-    body_tag = soup.body
-    if body_tag:
-        tags = body_tag.find_all(['a', 'img', 'form'])
-        for tag in tags:
-            if tag.has_attr('href'):
-                href = tag['href']
-                href = re.sub(r'https://web\.archive\.org/web/\d+/', '', href)
-                href = re.sub(r'https://web\.archive\.org/web/\d+im_/', '', href)
-                href = re.sub(r'/web/\d+/(http\S+)/', r'\1', href) # вид <a href="/web/20140317005012/http://
-                tag['href'] = href
-            if tag.has_attr('src'):
-                src = tag['src']
-                src = re.sub(r'https://web\.archive\.org/web/\d+/', '', src)
-                src = re.sub(r'https://web\.archive\.org/web/\d+im_/', '', src)
-                src = re.sub(r'/web/\d+/(http\S+)/', r'\1', src) 
-                tag['src'] = src
-            if tag.has_attr('action'):
-                action = tag['action']
-                action = re.sub(r'https://web\.archive\.org/web/\d+/', '', action)
-                action = re.sub(r'https://web\.archive\.org/web/\d+im_/', '', action)
-                action = re.sub(r'/web/\d+/(http\S+)/', r'\1', action) 
-                tag['action'] = action
 
     return soup
 
