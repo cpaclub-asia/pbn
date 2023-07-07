8c4f89f93455c0a25bf0c1f6dbd27a16f5e8b050
Anton Dodonov <cpodimas@gmail.com>
2023-07-07 16:10:31 +0700

    c:

--- content/nginx/2_make_upload.sh
+++ content/nginx/2_make_upload.sh
@@ -16,12 +16,12 @@ while IFS= read -r line; do
     # Извлечение имени домена до первого пробела или точки с запятой
     DOMAIN=$(echo "$line" | awk -F '[ ;]' '{print $1}')
 
-    /bin/mkdir -p data/sites-data/upload/$DOMAIN
-    cp -r ./data/sites-data/$DOMAIN/cleaned/all/ ./data/sites-data/upload/$DOMAIN
-    cp -r ./data/sites-data/$DOMAIN/assets/all/ ./data/sites-data/upload/$DOMAIN
+    /bin/mkdir -p data/upload-data/upload/$DOMAIN
+    cp -r ./data/sites-data/$DOMAIN/cleaned/all/ ./data/upload-data/upload/$DOMAIN
+    cp -r ./data/sites-data/$DOMAIN/assets/all/ ./data/upload-data/upload/$DOMAIN
     
 done < "$file"
 
-    CMD="tar zcvf data/sites-data/upload.tar.gz data/sites-data/upload/*"
+    CMD="tar zcvf data/upload-data/upload.tar.gz data/upload-data/upload/*"
     echo $CMD
     $CMD
--- content/webarchive/funcs/webarchive/html_utils.py
+++ content/webarchive/funcs/webarchive/html_utils.py
@@ -15,11 +15,12 @@ def remove_web_archive_links(soup):
     for tag in tags:
         for attr, value in tag.attrs.items():
             if isinstance(value, str):
-                updated_value = re.sub(r'https://web\.archive\.org/web/\d+/', '', value)
+                updated_value = value
+                updated_value = re.sub(r'https://web\.archive\.org/web/\d+/', '', updated_value)
                 updated_value = re.sub(r'https://web\.archive\.org/web/\d+im_/', '', updated_value)
                 updated_value = re.sub(r'https://web\.archive\.org/web/\d+cs_/', '', updated_value)
-                #https://web.archive.org/web/20230130234931im_
-                updated_value = re.sub(r'//web\.archive\.org/web/\d+/', '', value)
+                
+                updated_value = re.sub(r'//web\.archive\.org/web/\d+/', '', updated_value)
                 updated_value = re.sub(r'//web\.archive\.org/web/\d+im_/', '', updated_value)
                 updated_value = re.sub(r'//web\.archive\.org/web/\d+cs_/', '', updated_value)
 
@@ -63,7 +64,7 @@ def remove_some(file_name,input_dir,output_dir,relative_dir):
     html_file = os.path.join(input_dir,relative_dir,file_name)
 
     try:
-        with open(html_file, 'r') as file:
+        with open(html_file, 'r', encoding='utf-8', errors='ignore') as file:
             html_content = file.read()
 
         soup = BeautifulSoup(html_content, 'html.parser')
@@ -71,12 +72,13 @@ def remove_some(file_name,input_dir,output_dir,relative_dir):
         removed_html = remove_web_archive_links(soup)
         removed_html2 = remove_scripts(removed_html)
         removed_html3 = remove_comments(removed_html2)
+        final=str(removed_html3)
 
         output_subdir = os.path.join(output_dir,relative_dir)
         os.makedirs(output_subdir, exist_ok=True)
         file = os.path.join(output_subdir, os.path.basename(html_file))
         with open(file, 'w') as file:
-            file.write(str(removed_html3))
+            file.write(final)
     except Exception as e:
         print(f"ERROR!!!! An error occurred while processing file: {html_file}")
         print(str(e))
