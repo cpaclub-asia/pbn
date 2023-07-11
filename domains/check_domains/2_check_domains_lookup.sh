f1880a61a1d0cdcc1f0af236775038715c69b1fb
Anton Dodonov <anton@Antons-MacBook-Pro.local>
2023-07-11 20:40:49 +0700

    c:

--- domains/check_domains/2_check_domains_lookup.sh
+++ domains/check_domains/2_check_domains_lookup.sh
@@ -14,17 +14,19 @@
 
 
 INPUT_DIR=data/cc-data.n/domains.com
-OUTPUT_DIR=data/cc-data.n/domains.com.lookup
+OUTPUT_DIR1=data/cc-data.n/domains.com.noconnect
+OUTPUT_DIR2=data/cc-data.n/domains.com.connect
 
-mkdir -p $OUTPUT_DIR
+mkdir -p $OUTPUT_DIR1
+mkdir -p $OUTPUT_DIR2
 
 for file in $INPUT_DIR/*; do
   base=$(basename "$file")
   echo $base
   
   SRC=$file
-  DST1=$OUTPUT_DIR/$base.noconnect
-  DST2=$OUTPUT_DIR/$base.connect
+  DST1=$OUTPUT_DIR1/$base.noconnect
+  DST2=$OUTPUT_DIR2/$base.connect
 
   python3 -m check_domains_lookup $SRC $DST1 $DST2
 done
