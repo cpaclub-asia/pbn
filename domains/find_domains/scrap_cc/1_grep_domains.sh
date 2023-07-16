8200c2285f69be336aee6c13bb8df9211306d8c1
Your Name <you@example.com>
2023-07-16 10:10:55 +0300

    c:

--- domains/find_domains/scrap_cc/1_grep_domains.sh
+++ domains/find_domains/scrap_cc/1_grep_domains.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-INPUT_DIR=data/cc-data.r/current
-OUTPUT_DIR=data/cc-data.n/predomains.r
+#for macos replace grep to ggrep (-oP)
+
+INPUT_DIR=data/cc-data/current
+OUTPUT_DIR=data/cc-data/predomains.r
 
 mkdir -p $OUTPUT_DIR
 
 for file in $INPUT_DIR/*; do
   base=$(basename "$file")
   echo $base
-  grep '"status": "200"' "$file" | grep '"mime": "text/html"' | ggrep -oP '(?<="url": "https://)[^/]*' > "$OUTPUT_DIR/$base"
+  grep '"status": "200"' "$file" | grep '"mime": "text/html"' | grep -oP '(?<="url": "https://)[^/]*' > "$OUTPUT_DIR/$base"
 done
