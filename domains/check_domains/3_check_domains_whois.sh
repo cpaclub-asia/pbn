706345b1e5cf332536026155efab524c7456318b
Your Name <you@example.com>
2023-07-20 16:14:04 +0300

    c:

--- domains/check_domains/3_check_domains_whois.sh
+++ domains/check_domains/3_check_domains_whois.sh
@@ -1,13 +1,14 @@
 
 #data/cc-data.n/domains
-INPUT_DIR=data/domains-data/noconnect/cc-2023-06/com
-OUTPUT_DIR=data/domains-data/noconnect.whois/cc-2023-06/com
-WORKERS=5
+INPUT_DIR=data/domains-data/noconnect/cc-2023-06/nocom
+OUTPUT_DIR=data/domains-data/noconnect.whois/cc-2023-06/nocom
+WORKERS=10
 
 mkdir -p $OUTPUT_DIR
 
 
 process_file() {
+
   file="$1"
   base=$(basename "$file")
   echo "$base"
@@ -15,7 +16,7 @@ process_file() {
   SRC1="$file"
   DST1="$OUTPUT_DIR/$base.whois"
   
-  python3 -m check_domains_whois -f $SRC1 -c -i 0 -oe --track-whois-text-changes --workers 5 --csv $DST1
+  python3 -m check_domains_whois -f $SRC1 -c -i 0 -oe --track-whois-text-changes --workers $WORKERS --csv $DST1
 
 }
 
