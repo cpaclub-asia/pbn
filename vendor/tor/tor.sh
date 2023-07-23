4dfb134614c88081a2885b79269f709b8839b983
Your Name <you@example.com>
2023-07-23 12:41:22 +0300

    c:

--- vendor/tor/tor.sh
+++ vendor/tor/tor.sh
@@ -1,11 +1,17 @@
 #sudo apt-get update
 #sudo apt-get install tor torsocks
 
+#tor -f torrc.1
+#exit 0
+
+
 #sudo service tor start
-sudo tor -f torrc.2 >> ../../../log/torrc.2 &
-sudo tor -f torrc.3 >> ../../../log/torrc.3 & 
-sudo tor -f torrc.4 >> ../../../log/torrc.4 &
-sudo tor -f torrc.5 >> ../../../log/torrc.5 &
-sudo tor -f torrc.6 >> ../../../log/torrc.6 &
-sudo tor -f torrc.7 >> ../../../log/torrc.7 &
-sudo tor -f torrc.1 >> ../../../log/torrc.1 &
+nohup tor -f torrc.1 >> data/logs/torrc.1 &
+nohup tor -f torrc.2 >> data/logs/torrc.2 &
+nohup tor -f torrc.3 >> data/logs/torrc.3 & 
+nohup tor -f torrc.4 >> data/logs/torrc.4 &
+nohup tor -f torrc.5 >> data/logs/torrc.5 &
+nohup tor -f torrc.6 >> data/logs/torrc.6 &
+nohup tor -f torrc.7 >> data/logs/torrc.7 &
+nohup tor -f torrc.8 >> data/logs/torrc.8 &
+
