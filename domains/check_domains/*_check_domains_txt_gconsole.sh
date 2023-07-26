a9472254aace21e4e13acf036f54cac07a8daabc
ginterloper <ginterloper@gmail.com>
2023-07-26 13:21:46 +1000

    Create *_check_domains_txt_gconsole.sh

---
+++ domains/check_domains/*_check_domains_txt_gconsole.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+check_txt_records() {
+		local domain=$1
+		local dns_server="8.8.8.8"  # Google Public DNS сервер
+		local txt_records=$(nslookup -q=txt $domain $dns_server | grep "text =")
+		
+		if [ -n "$txt_records" ]; then
+				#echo "TXT records for $domain:"
+				#echo "$txt_records"
+				if [[ $txt_records == *'text = "google-site-verification='* ]]; then
+				echo "Found Google Console TXT record"
+				fi
+		else
+				echo "TXT records for $domain not found."
+		fi
+}
+
+check_txt_records "iteching.info"
